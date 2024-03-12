054853eecff112a9ccbf08ed7e7e5456e2adf7ae
maisajulianna <maisa.osterman@gmail.com>
2024-03-12 11:21:52 +0200

    moved files to new branch

--- web7-main/frontend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to MongoDB");
-};
-
-module.exports = connectDB;
